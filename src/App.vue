fb830b6e5850b628b98ea9137d450ad2d9636308
sahil2760 <sahilvengurlekar50@gmail.com>
2021-02-13 12:38:41 +0530

    assignment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.jpeg">
+  <HelloWorld msg="Get Back to Living"/>
+  <HelloWorld msg="Cyzplamovir ould be available to everyone o needs it. Cant afford it? Enroll here to see if we can help. "/>
 </template>
 
 <script>
@@ -19,8 +20,17 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#styled-input {
+  height: 40px;
+  font-size: 20pt;
+}
+.styled-input label[for] {
+  height: 40px;
+  font-size: 20pt;
 }
 </style>
