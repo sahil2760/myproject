ab9fdcc214388a271cd97f4c179efc37507df830
sahil2760 <sahilvengurlekar50@gmail.com>
2021-02-18 13:39:10 +0530

    debugging

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template>
-  <!-- <text1 msg1="Get Back to Living"/> -->
-  <!-- <v-img class="shrink mr-2" contains src="./assets/logo.jpeg" alt="Vue logo"> -->
-  <!-- <text2 msg2="Cyzplamovir should be available to everyone o needs it. Cant afford it? Enroll here to see if we can help."/> -->
-  <div id="app">
-  {{ message }}
-</div>
+  <img alt="logo" src="./assets/logo.jpeg">
+  <HelloWorld msg="Cyzplamovir should be available to everyone who needs it. Can't afford it? Enroll to see if we can help."/>
 </template>
 
-
 <script>
-// import text1 from './components/HelloWorld.vue'
-// import text2 from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-// export default {
-//   name: 'App',
-//   components: {
-//     text1 ,
-//     text2 
-//   }
-// }
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
   }
-})
+}
 </script>
 
 <style>
@@ -33,9 +19,8 @@ var app = new Vue({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c55c5;
+  text-align: center;
+  color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="hello" style="margin: 20%">
-    <h1>Get Back to Living</h1>
-    <h2>
-      Cyzplamovir should be available to everyone who needs it. Can afford it?
-      Enroll to see if we can help.
-    </h2>
-    <label class="label">Float label type auto</label>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+
     <input
       v-model="message"
       id="styled-input"
@@ -52,33 +48,22 @@
     <input v-model="message" placeholder="Email" required /> <br />
     <p>Question?<a v-bind:href="www.google.com"> talk with us </a></p>
     <br />
-    <div class="form-group col-sm-6" style="padding: 10% 22%">
-      <button class="btn-primary" style="width: 100px; font-size: large">
-        Enroll
-      </button>
-    </div>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: "text1",
-  text1: {
-    msg1: "Cyzplamovir",
-  },
-  name1: "text2",
-  text2: {
-    msg2:
-      "Cyzplamovir should be available to everyonewho needs it. Can't afford it? enroll here to see if we can help",
-  },
-};
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
 </script>
 
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 a {
-  color: #8d4747;
+  color: #42b983;
 }
 </style>
-
-
