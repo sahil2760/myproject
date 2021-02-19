b815b76b07bc0041f17bd34d7dd71b132a6aa230
sahil2760 <sahilvengurlekar50@gmail.com>
2021-02-19 10:58:27 +0530

    my final commit

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.jpeg">
+    <HelloWorld msg="Cyzplamovir should be available to everyone who needs it. Can't afford it? Enroll to see if we can help."/>
   </div>
 </template>
 
--- project/src/components/HelloWorld.vue
+++ project/src/components/HelloWorld.vue
@@ -1,32 +1,50 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <input
+      v-model="message"
+      id="styled-input"
+      style="width: 20%; height: 20%"
+      placeholder="First Name"
+      required
+    />
+    <input
+      v-model="message"
+      id="styled-input"
+      style="width: 20%; height: 20%"
+      placeholder="Last Name"
+      required
+    />
+   
+    <br />
+    <input
+      v-model="message"
+      style="width: 40%; height :20%"
+      placeholder="Address 1"
+      required
+    /><br />
+    <input
+      v-model="message"
+      style="width: 40%; "
+      placeholder="Address 2"
+    /><br />
+  
+    <select v-model="selected">
+      <option disabled value="0">State</option>
+      <option>Maharastra</option>
+      <option>Gujrat</option>
+      <option>Goa</option>
+      <option>rajasthan</option>
+      <option>delhi</option>
+      <option>odisha</option>
+      <option>assam</option>
+      <option>Meghalaya</option>
+      <option>West Bengal</option>
+    </select>
+    <input v-model="message" placeholder="Zipcode"  required />
+    <input v-model="message" placeholder="Email" required /> <br />
+    <p>Questions?<a href="https://prottapp.com/p/6c1b1b#/s/5f038f5686e465379996195b">Talk to us?</a></p>  
+  
   </div>
 </template>
 
@@ -41,18 +59,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
-  color: #42b983;
+  color: #dc5f29;
 }
 </style>
